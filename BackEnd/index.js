34bacce856ae26fc5048c8832f382dc22b171da1
ruanpabl01 <ruangomes.5330@aluno.saojudas.br>
2022-10-09 18:41:08 -0300

    Segundo Micro servico e Esqueleto FrontEnd
    Implementação do segundo micro serviço e esqueleto do front-end

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -5,9 +5,6 @@ const axios = require('axios');
 //Lista de medicamentos (É incrementada a cada novo medicamento).
 const medicamentos = {};
 
-//Nova lista de medicamentos, é populada apenas com medicamentos específicos. Serve para o GET (READ).
-const novaLista = {};
-
 //ID do medicamento (é incrementado a cada novo medicamento).
 let idMedicamento = 0;
 
@@ -42,27 +39,52 @@ app.get('/medicamentos', (req, res) => {
   //Atribui os parâmetros da requisição (POSTMAN) à variável entre chaves.
   //Nesse caso o que é passado via requisição é: O nome do medicamento cujo qual se deseja obter uma lista.
   const { nomeMedicamentoParaConsulta } = req.body;
+ 
+  //Nova lista de medicamentos, é populada apenas com medicamentos específicos. Serve para o GET (READ).
+  const novaLista = {};
+
   //Percorre toda a lista de medicamentos existente
   for (let cont = 1; cont <= idMedicamento; cont++) {
-    //Lógica IF para verificar quais chaves possuem o nomeMedicamento = nomeMedicamentoParaConsulta.
-    if (medicamentos[cont].nomeMedicamento === nomeMedicamentoParaConsulta) {
-      //Inclui os atributos que possuem o mesmo nome a uma nova lista.    
-      novaLista[cont] = medicamentos[cont];
+    //Quando se está percorrendo a lista e passa por um ID deletado, ocorre uma exceção. Por isso o try/catch.
+    try {
+      //Lógica IF para verificar quais chaves possuem o nomeMedicamento = nomeMedicamentoParaConsulta.
+      if (medicamentos[cont].nomeMedicamento === nomeMedicamentoParaConsulta) {
+        //Inclui os atributos que possuem o mesmo nome a uma nova lista.    
+        novaLista[cont] = medicamentos[cont];
+      }
+    } catch (err) {
+      delete (novaLista[idMedicamento]);
     }
   }
+
   //Retorna a nova lista.
   res.status(200).send(novaLista)
+
 })
 
 //PUT para atualizar um a quantidade disponível de um medicamento de acordo com o ID passado (UPDATE).
 app.put('/medicamentos', async (req, res) => {
   //Atribui os parâmetros da requisição (POSTMAN) às variáveis que estão entre chaves.
-  //Nesse caso o que é passado via requisição é: O nome do medicamento cujo qual se deseja obter uma lista.
-  const { idMedicamentoParaAtualizar, novaQuantidadeDisponivel } = req.body;
-  //De acordo com o ID do medicamento enviado no request, substitui a quantidade disponível na lista pela nova quantidade enviada no request.
-  medicamentos[idMedicamentoParaAtualizar].quantidadeDisponivel = novaQuantidadeDisponivel;
-  //Resposta de sucesso no UPDATE
-  res.status(201).send("Quantidade do medicamento ID: " + idMedicamentoParaAtualizar + " atualizada.");
+  //Nesse caso o que é passado via requisição é: O id do medicamento cujo qual se deseja subtrair e a quantidade.
+  const { idMedicamentoParaAtualizar, quantidadeSubtrair } = req.body;
+  try {
+    //De acordo com o ID do medicamento enviado no request, faz a subtração da quantidade atual pela quantidade enviada no request.
+    medicamentos[idMedicamentoParaAtualizar].quantidadeDisponivel -= quantidadeSubtrair;
+    //Se a quantidade estiver entre 0 e 3, emite um alerta de que o estoque do medicamento está acabando.
+    if (medicamentos[idMedicamentoParaAtualizar].quantidadeDisponivel <= 3 && medicamentos[idMedicamentoParaAtualizar].quantidadeDisponivel > 0) {
+      res.status(201).send("Atenção! O medicamento " + idMedicamentoParaAtualizar + " está com o estoque acabando! Restam: " + medicamentos[idMedicamentoParaAtualizar].quantidadeDisponivel);
+    }
+    //Se a quantidade do medicamento for igual a 0, emite um alerta de que o estoque do medicamento acabou.
+    else if (medicamentos[idMedicamentoParaAtualizar].quantidadeDisponivel === 0) {
+      res.status(201).send("Atenção! O estoque do medicamento: " + idMedicamentoParaAtualizar + " acabou.");
+    } else {
+      //Resposta de sucesso no UPDATE
+      res.status(201).send("Quantidade do medicamento ID: " + idMedicamentoParaAtualizar + " atualizada.");
+    }
+    //Quando tenta atualizar um medicamento que foi deletado, lança uma exception e retorna para o usuário de que o medicamento foi deletado ou não existe.
+  } catch (err) {
+    res.status(201).send("O ID: " + idMedicamentoParaAtualizar + " não existe ou foi deletado.");
+  }
 })
 
 
